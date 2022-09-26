f6e1ad77711c9f7f5ca4786cc7e83883585b6212
loragonzaleze <61980534+loragonzaleze@users.noreply.github.com>
2022-09-26 16:11:34 -0400

    Barebones backend, added gitignore, basic routes

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+/** Dependencies used by the app. */
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+
+/** Route files */
+const test = require('./routes/api/sample')
+
+/** Connect routes to app. */
+app.use('/api/', test)
+
+
+const port = process.env.port || 5002;
+
+app.listen(port, () => {
+    console.log('Server has started on port ' + port);
+    console.log('listening at http://localhost:' + port)
+})
+
