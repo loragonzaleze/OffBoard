b56d2098b9dc1b515045f37ec39e94568d013e64
wuseokjung <wuseok.jung@gmail.com>
2022-11-07 11:23:52 -0500

    colour fixes

--- frontend/onboard/src/components/talent-entry.js
+++ frontend/onboard/src/components/talent-entry.js
@@ -1,4 +1,4 @@
-import './stylesheets/talent-entry.css'
+import './stylesheets/talent-entry.css';
 import React, { useState, useEffect } from 'react';
 import { BsLinkedin } from 'react-icons/bs';
 // Row in main talent page
@@ -11,63 +11,82 @@ import { BsLinkedin } from 'react-icons/bs';
 // linkedin: linkedin link
 // location: ex. St Petersburg, FL
 function TalentEntry(props) {
-  const [containerColor, setContainerColor] = useState("#fff")
-  const [years, setYears] = useState("");
-  const [yearColor, setYearColor] = useState("");
-  const [fieldColor, setFieldColor] = useState("#fff");
+	const [containerColor, setContainerColor] = useState('#fff');
+	const [years, setYears] = useState('');
+	const [yearColor, setYearColor] = useState('');
+	const [fieldColor, setFieldColor] = useState('#fff');
 
-  // TODO Finish filling in the colors for all Job fields
-  const fieldToColor = new Map([
-    ['Engineering', '#ffe4b2'],
-    ['Product', '#ffcccc'],
-    ['Business', '#ccffcc ']
-  ]); 
-  useEffect(() => {
-    // Set Year Tag info
-    let displayYears = "Y"
-    if(props.years > 9) {
-      displayYears += "10+"
-    }
-    else {
-      displayYears += props.years
-    }
-    setYears(displayYears);
-    const colors = [
-      "#e9f2fa", "#e9f2fa", "#bedaf0", "#93c2e7", "#7eb6e2",
-       "#68aade", "#529ed9", "#529ed9", "#3d92d4", "#3d92d4"
-    ];
-    setYearColor(colors[props.years - 1]);
+	// TODO Finish filling in the colors for all Job fields
+	const fieldToColor = new Map([
+		['Engineering', '#ffe4b2'],
+		['Product', '#ffcccc'],
+		['Business', '#ccffcc '],
+	]);
+	useEffect(() => {
+		// Set Year Tag info
+		let displayYears = 'Y';
+		if (props.years > 9) {
+			displayYears += '10+';
+		} else {
+			displayYears += props.years;
+		}
+		setYears(displayYears);
+		const colors = [
+			'#e9f2fa',
+			'#e9f2fa',
+			'#bedaf0',
+			'#93c2e7',
+			'#7eb6e2',
+			'#68aade',
+			'#529ed9',
+			'#529ed9',
+			'#3d92d4',
+			'#3d92d4',
+		];
+		setYearColor(colors[props.years - 1]);
 
-    setFieldColor(fieldToColor.get(props.field));
+		setFieldColor(fieldToColor.get(props.field));
 
-    if(props.alt) {
-      setContainerColor("#d3d3d350");
-    } else {
-      setContainerColor("#fff")
-    }
-  });
-  return (
-    <div className = "talent-entry-container" style = {{backgroundColor: containerColor}}>
-      <text className = "talent-entry-name">{props.name}</text>
-      <text className = "talent-entry-font">{props.job}</text>
-      <div className = "talent-entry-tag-years" style = {{backgroundColor: yearColor}}>
-        {years}
-      </div>
-      <text className = "talent-entry-company">{props.company}</text>
-      <div className = "talent-entry-tag-field-container">
-        <div className = "talent-entry-tag-field" style = {{backgroundColor: fieldColor}}>
-            {props.field}
-        </div>
-      </div>
-      <div className="talent-entry-resume">
-        <a href={props.resume} download>Resume</a>        
-      </div>
-      <BsLinkedin className = "talent-entry-linkedin" 
-      onClick = {() => window.open(props.linkedin, '_blank')}/>
-      <div className = "talent-entry-location">{props.location}</div>
-
-    </div>
-  );
+		if (props.alt) {
+			setContainerColor('#F6F6F6');
+		} else {
+			setContainerColor('#fff');
+		}
+	});
+	return (
+		<div
+			className="talent-entry-container"
+			style={{ backgroundColor: containerColor }}
+		>
+			<text className="talent-entry-name">{props.name}</text>
+			<text className="talent-entry-font">{props.job}</text>
+			<div
+				className="talent-entry-tag-years"
+				style={{ backgroundColor: yearColor }}
+			>
+				{years}
+			</div>
+			<text className="talent-entry-company">{props.company}</text>
+			<div className="talent-entry-tag-field-container">
+				<div
+					className="talent-entry-tag-field"
+					style={{ backgroundColor: fieldColor }}
+				>
+					{props.field}
+				</div>
+			</div>
+			<div className="talent-entry-resume">
+				<a href={props.resume} download>
+					Resume
+				</a>
+			</div>
+			<BsLinkedin
+				className="talent-entry-linkedin"
+				onClick={() => window.open(props.linkedin, '_blank')}
+			/>
+			<div className="talent-entry-location">{props.location}</div>
+		</div>
+	);
 }
 
 export default TalentEntry;
